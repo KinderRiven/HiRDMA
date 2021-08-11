2cb83986e986655be116618156d0a0c4d230d2b6
KinderRiven <1339764596@qq.com>
2021-08-11 16:49:27 +0800

    a simple push by kinderriven

--- test/example/demo_1.cpp
+++ test/example/demo_1.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-08-11 16:43:34
- * @LastEditTime: 2021-08-11 16:44:24
+ * @LastEditTime: 2021-08-11 16:49:22
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /HiRDMA/test/example/demo_1.cpp
@@ -11,7 +11,8 @@
 
 int main(int argc, char** argv)
 {
+    hi_rdma::Options _options;
     hi_rdma::HiRDMA* _hi_rdma = nullptr;
-    hi_rdma::HiRDMA::CreateRDMAContext(_hi_rdma);
+    hi_rdma::HiRDMA::CreateRDMAContext(_options, &_hi_rdma);
     return 0;
 }
