9b8683add944cef4825bb55a0655b69118d77f65
KinderRiven <1339764596@qq.com>
2021-08-12 18:19:20 +0800

    a simple push by kinderriven

--- test/example/demo_1.cpp
+++ test/example/demo_1.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-08-11 16:43:34
- * @LastEditTime: 2021-08-12 18:08:27
+ * @LastEditTime: 2021-08-12 18:19:13
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /HiRDMA/test/example/demo_1.cpp
@@ -16,8 +16,10 @@ int main(int argc, char** argv)
     hi_rdma::Status _status = hi_rdma::HiRDMA::CreateRDMAContext(_options, &_hi_rdma);
     if (_status.ok()) {
         printf("CreateRDMAContext Success!\n");
+        printf("%s\n", _status.ToString().c_str());
     } else {
         printf("CreateRDMAContext Failed!\n");
+        printf("%s\n", _status.ToString().c_str());
     }
     return 0;
 }
