0f1bbde8e827415daab54642445dcd82a6bda6a8
KinderRiven <1339764596@qq.com>
2021-08-17 15:54:23 +0800

    a simple push by kinderriven

--- test/cs/config.hpp
+++ test/cs/config.hpp
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-08-17 15:02:33
- * @LastEditTime: 2021-08-17 15:08:26
+ * @LastEditTime: 2021-08-17 15:54:18
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /HiRDMA/test/cs/config.hpp
@@ -10,7 +10,7 @@
 #ifndef ICNLUDE_CONFIG_HPP_
 #define INCLUDE_CONFIG_HPP_
 
-const char server_ip[] = "10.18.13.42";
+const char server_ip[] = "172.18.13.42";
 
 const char server_port[] = "4396";
 
