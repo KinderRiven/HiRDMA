d861b70e768690c7f730dd390d161cd7268879b1
KinderRiven <1339764596@qq.com>
2021-08-18 17:32:54 +0800

    a simple push by kinderriven

--- src/hi_rdma.cc
+++ src/hi_rdma.cc
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-08-11 15:44:55
- * @LastEditTime: 2021-08-18 16:56:49
+ * @LastEditTime: 2021-08-18 17:32:51
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /HiRDMA/src/hi_rdma.cpp
@@ -200,6 +200,7 @@ Status HiRDMA::PollQP(int num)
             printf("  [byte_len:%d][qp_num:%d][src_qp:%d]\n", wc.byte_len, wc.qp_num, wc.src_qp);
             return Status::IOError("PollQP Failed.");
         } else if ((n) && (wc.status == IBV_WC_SUCCESS)) {
+            printf("%d\n", n);
             num -= n;
         }
         if (num <= 0) {
